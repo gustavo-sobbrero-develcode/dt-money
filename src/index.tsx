503827cc988cbda20615a126e51bef0e643bd79b
gustavo-sobbrero-develcode <gustavo.sobbrero@develcode.com.br>
2022-10-06 10:48:57 -0300

    dtmoney mvp

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freela de app",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2022-10-05 09:32:11"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2022-10-02 09:32:11"),
+        },
+        {
+          id: 3,
+          title: "Conta de energia",
+          type: "withdraw",
+          category: "Casa",
+          amount: 300,
+          createdAt: new Date("2022-10-04 09:34:11"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
